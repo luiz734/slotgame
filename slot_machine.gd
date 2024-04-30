689ebbfa30846c2e08a0475151d900b76847ce2b
Luiz G. <luiz007@live.com>
2024-04-30 14:31:22 -0300

    Remove AnimationPlayer to use Tweens instead for spin animations

--- slot_machine.gd
+++ slot_machine.gd
@@ -1,66 +1,82 @@
 extends Node2D
 @export var tile_prefab: PackedScene
-
-@export var n_tiles = 3
-@export var speed := 500
+@export var n_tiles = 4
+@export var speed := 128
 @export var distance_before_reset := 300
 @export var impulse_distance_before_play = -50
 @export var vertical_separation = 20
 @export var sprite_size_y = 128
 
-enum State {running, stopped}
+enum State {spinning, stopped}
 var state
 var base_position_y = 0.0
 var _tiles: Array[Node]
 var tiles_moved = 2
 
-func get_color(i):
-    if i == 0: return Color.RED
-    if i == 1: return Color.GREEN
-    if i == 2: return Color.BLUE
-func get_n(i):
-    if i == 0: return "RED"
-    if i == 1: return "GREEN"
-    if i == 2: return "BLUE"
-func _ready():
-    assert(tile_prefab)
-    state = State.stopped
-    
-    for i in range(n_tiles):
-        var tile = tile_prefab.instantiate()
-        tile.modulate = get_color(i)
-        _tiles.push_back(tile)
-        tile.position = Vector2(0, -i * (sprite_size_y))
-        add_child(tile)
-        tile.name = get_n(i)
-        tile.moved.connect(func(t: SlotTile):
-            if t.position.y > 64:
-                    t.position = Vector2(0, -sprite_size_y * (n_tiles - 1))
-                    tiles_moved += 1
-            if state == State.running:
-                t.move_by(Vector2(0, sprite_size_y))
-            else:
-                t.spin_down()
-   
-        )
-        
-func spin():
-    for i in range(0, len(_tiles)):
-        _tiles[i].spin_up()
-    await get_tree().create_timer(0.3).timeout
-    for tile in _tiles:
-        tile.move_by(Vector2(0, sprite_size_y))
-       
-    
+var tiles_per_sec: float = 8
+var movement_duration: float = 1.0 / tiles_per_sec
+var tile_y_separation: float = 32.0
 
+func _ready():
+    assert(tile_prefab, "missing tile prefab")
+    state = State.stopped
+    _setup_tiles_before_first_spin()
+    
+    
 func _process(delta):
     if Input.is_action_just_pressed("run"):
         if state == State.stopped:
-            spin()
-            state = State.running
+            _spin()
+            state = State.spinning
         else:
             print(_tiles[tiles_moved % n_tiles].name)
             state = State.stopped
 
-func _physics_process(delta):
-    pass
+# debug only
+func D_get_color(i):
+    if i == 0: return Color.RED
+    if i == 1: return Color.GREEN
+    if i == 2: return Color.BLUE
+    if i == 3: return Color.BLACK
+func D_get_n(i):
+    if i == 0: return "RED"
+    if i == 1: return "GREEN"
+    if i == 2: return "BLUE"
+    if i == 3: return "BLACK"
+
+    
+# Called after each individual tile move
+# It will keep move the tile or runs the spin_down animation
+func _on_tile_moved(t: SlotTile):
+    var over_max_y = t.position.y > 128+64
+    if over_max_y:
+        t.position = Vector2(0, -sprite_size_y * (n_tiles - 2) - 50)
+        tiles_moved += 1
+    if state == State.spinning:
+        t.move_by(Vector2(0, sprite_size_y), movement_duration)
+    else:
+        t.spin_down(movement_duration)  
+
+func _setup_tiles_before_first_spin():
+    for i in range(n_tiles):
+        var tile = tile_prefab.instantiate()
+        _tiles.push_back(tile)
+        tile.position = Vector2(0, -i * (sprite_size_y))
+        add_child(tile)
+        tile.moved.connect(_on_tile_moved)
+        tile.set_animation_speed_scale(tiles_per_sec * 0.4)
+        # debug only
+        tile.modulate = D_get_color(i)
+        tile.name = D_get_n(i)
+        
+func _spin():
+    # spin up animation
+    for i in range(1, len(_tiles)):
+        _tiles[i].spin_up(movement_duration)
+    await _tiles[0].spin_up(movement_duration)
+    # hardcode the amount before actual spinning
+    #await get_tree().create_timer(1.0).timeout
+    # spin all tiles
+    # they will keep spinning until state change to "stopped"
+    for tile in _tiles:
+        tile.move_by(Vector2(0, sprite_size_y), movement_duration)
--- slot_tile.gd
+++ slot_tile.gd
@@ -5,26 +5,38 @@ signal moved(tile)
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 func _ready():
-    animation_player.speed_scale = 1.0 / (0.3 * 2.0)
+    pass
+    
+func set_animation_speed_scale(scale: float) -> void:
+    animation_player.speed_scale = scale
 
-func move_to(to: Vector2):
+func move_to(to: Vector2, duration_sec: float):
     var tween: Tween = get_tree().create_tween()
     tween.set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_LINEAR)
-    tween.tween_property(self, "position", to, 0.3)
+    tween.tween_property(self, "position", to, duration_sec)
     await tween.finished
     moved.emit(self)
 #
 #func spin(texture_size: float):
     #tile.move_by(Vector2(0, sprite_size_y))
 
-func move_by(by: Vector2):
-  move_to(position + by)
+func move_by(by: Vector2, duration_sec: float):
+  move_to(position + by, duration_sec)
   
-func spin_up():
-    animation_player.play("spin_up")
-    await animation_player.animation_finished
+func spin_up(duration_sec: float):
+    var to = position - Vector2(0, 50.0)
+    var tween: Tween = get_tree().create_tween()
+    tween.set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_LINEAR)
+    tween.tween_property(self, "position", to, duration_sec)
+    await tween.finished
+    #animation_player.play("spin_up")
+    #await animation_player.animation_finished
     
-func spin_down():
-    animation_player.play("spin_down")
-    await animation_player.animation_finished
+func spin_down(duration_sec: float):
+    var to = position - Vector2(0, 128.0 - 50.0)
+    var tween: Tween = get_tree().create_tween()
+    tween.set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_LINEAR)
+    tween.tween_property(self, "position", to, duration_sec)
+    await tween.finished
+
 
