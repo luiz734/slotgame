4269d4e921b48568b2afcf832acdf3dab0fb95e4
Luiz G. <luiz007@live.com>
2024-05-01 12:20:17 -0300

    Add movement control from Wheel to all SlotTiles instead of individual

--- wheel.gd
+++ wheel.gd
@@ -2,18 +2,23 @@ extends Node2D
 signal wheel_stopped()
 
 @export var tile_prefab: PackedScene
-@export var speed := 128
+@export var speed: int = 32
 @export var distance_before_reset := 300
 @export var impulse_distance_before_play = -50
 @export var vertical_separation = 20
 @export var sprite_size_y = 128
 @export var values: Array[String] = ["easy", "medium", "hard", "very hard"]
 
+@onready var D_glass = $DebugGlass
+const BELLOW_VISIBLE = 1
+
+var tiles_base_y_position: Array[int]
+
 enum State {spinning, stopped}
 var state
 var base_position_y = 0.0
 var _tiles: Array[Node]
-var tiles_moved = 2
+var current_visible = 2
 var n_tiles = -1
 
 var tiles_per_sec: float = 1
@@ -21,85 +26,52 @@ var movement_duration: float = 1.0 / tiles_per_sec
 var tile_y_separation: float = 32.0
 
 func _ready():
-    test_swap_random_tiles()
     assert(tile_prefab, "missing tile prefab")
     n_tiles = len(values)
-    #assert(len(values) == n_tiles, "len(values) should match n_tiles")
-    state = State.stopped
+    tiles_base_y_position = get_tiles_position_y(sprite_size_y, vertical_separation, n_tiles)
+    state = State.spinning
+    
+    D_glass.size = Vector2((sprite_size_y + 2*vertical_separation) * 2, sprite_size_y + 2*vertical_separation)
+    D_glass.position.x = -(sprite_size_y + 2*vertical_separation) 
+    D_glass.position.y = get_visible_y_position() + (sprite_size_y + 2*vertical_separation) / 2.0 - vertical_separation
+    
     _setup_tiles_before_first_spin()
-    
-    
-func _process(delta):
-    pass
-
-# debug only
-func D_get_color(i):
-    if i == 0: return Color.RED
-    if i == 1: return Color.GREEN
-    if i == 2: return Color.BLUE
-    if i == 3: return Color.BLACK
-func D_get_n(i):
-    if i == 0: return "RED"
-    if i == 1: return "GREEN"
-    if i == 2: return "BLUE"
-    if i == 3: return "BLACK"
-
-    
-# Called after each individual tile move
-# It will keep move the tile or runs the spin_down animation
-func _on_tile_moved(t: SlotTile):
-    var over_max_y = t.position.y > 128+128
-    if over_max_y:
-        t.position = Vector2(0, -sprite_size_y * (n_tiles - 2) - 50 + 128)
-        swap_random_tiles()
-        tiles_moved += 1
-    if state == State.spinning:
-        t.move_by(Vector2(0, sprite_size_y), movement_duration)
-    else:
-        t.spin_down(movement_duration)  
+   
+func get_tiles_position_y(sprite_size: int, vertical_separation: int, n_tiles: int) -> Array[int]:
+    var positions_y: Array[int] = []
+    for i in range(n_tiles):
+        var new_pos_y = (sprite_size + vertical_separation) * (i + 1)
+        # up is negative
+        new_pos_y = -new_pos_y
+        positions_y.push_back(new_pos_y)
+    return positions_y
 
 func _setup_tiles_before_first_spin():
     for i in range(n_tiles):
         var tile = tile_prefab.instantiate()
         _tiles.push_back(tile)
-        tile.position = Vector2(0, -i * (sprite_size_y) + sprite_size_y)
+        tile.position.y = tiles_base_y_position[i]
         tile.set_label_text(values[i])
         add_child(tile)
-        tile.moved.connect(_on_tile_moved)
         tile.name = values[i]
-        # debug only
-        #tile.modulate = D_get_color(i)
-        #tile.name = D_get_n(i)
 
-func get_current_tile_index() -> int:
-    return tiles_moved % n_tiles
-
-func test_swap_random_tiles():
-    return
-    print(get_swapable_indexes(4, 7), [3,2,1,0])
-    print(get_swapable_indexes(1, 7), [0,6,5,4])
-    assert(get_swapable_indexes(4, 7) == [3,2,1,0])
-    assert(get_swapable_indexes(1, 7) == [0,6,5,4])
-
-
-func get_swapable_indexes(before_index: int, n: int):
-    var swapable_indexes = []
-    var current_tile_index = get_current_tile_index()
+# TESTED
+func get_swapable_indexes(visible_tile_index: int, n_tiles: int) -> Array[int]:
+    var swapable_indexes: Array[int] = []
     var count = 0
-    var index = (current_tile_index - 2) % n_tiles
-    while count < n - 5:
-        index = index - 1
-        if index < 0:
-            index = n_tiles - 1
+    var index = (visible_tile_index + 1) % n_tiles
+    while count < n_tiles - 4:
+        index = (index + 1) % n_tiles
+        if index >= n_tiles:
+            index = 0
         swapable_indexes.push_back(index)
         count += 1
     print(swapable_indexes)
     return swapable_indexes 
     
-
 func swap_random_tiles():
     if n_tiles > 4:
-        var current_tile_index = get_current_tile_index()    
+        var current_tile_index = null   
         var swapable = get_swapable_indexes(current_tile_index, n_tiles)
         var index_a = swapable.pick_random()
         var index_b = swapable.pick_random()
@@ -111,16 +83,57 @@ func swap_random_tiles():
 
 func stop():
     state = State.stopped
+    speed = 32.0
        
 func spin():
     state = State.spinning
+    speed = 32.0
     # spin up animation
-    for i in range(1, len(_tiles)):
-        _tiles[i].spin_up(movement_duration * 2.0)
-    await _tiles[0].spin_up(movement_duration * 2.0)
+    #for i in range(1, len(_tiles)):
+        #_tiles[i].spin_up(movement_duration * 2.0)
+    #await _tiles[0].spin_up(movement_duration * 2.0)
     # hardcode the amount before actual spinning
     #await get_tree().create_timer(1.0).timeout
     # spin all tiles
     # they will keep spinning until state change to "stopped"
-    for tile in _tiles:
-        tile.move_by(Vector2(0, sprite_size_y), movement_duration)
+    #for tile in _tiles:
+        #tile.move_by(Vector2(0, sprite_size_y), movement_duration)
+    #move_wheel(movement_duration)
+
+func _physics_process(delta):
+    move_wheel(delta)
+
+func get_visible_y_position():
+    return tiles_base_y_position[BELLOW_VISIBLE + 1]
+
+func move_wheel(delta: float):
+    if state == State.spinning:
+        for i in range(n_tiles):
+            var t = _tiles[i]
+            t.position.y += delta * speed
+            if t.position.y > 0:
+                t.position.y = tiles_base_y_position[n_tiles - 1]
+                current_visible  = (current_visible + 1) % n_tiles
+            #swap_random_tiles()
+           
+
+
+#func move_by(by: Vector2, duration_sec: float):
+  #move_to(position + by, duration_sec)
+  
+#func spin_up(duration_sec: float):
+    #var to = position - Vector2(0, 50.0)
+    #var tween: Tween = get_tree().create_tween()
+    #tween.set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_LINEAR)
+    #tween.tween_property(self, "position", to, duration_sec)
+    #await tween.finished
+    ##animation_player.play("spin_up")
+    ##await animation_player.animation_finished
+    #
+#func spin_down(duration_sec: float):
+    #var to = position - Vector2(0, 128.0 - 50.0)
+    #var tween: Tween = get_tree().create_tween()
+    #tween.set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_LINEAR)
+    #tween.tween_property(self, "position", to, duration_sec)
+    #await tween.finished
+
